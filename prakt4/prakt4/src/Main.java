41badc4f0b0cdc579a9175c1fc3e7bd17662ccbe
artemkons <58778729+artemkons@users.noreply.github.com>
2020-11-08 19:38:20 +0300

    Add files via upload

---
+++ prakt4/prakt4/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Author author = new Author("Steven King", "king229@gmail.com",'M');
+        System.out.println(author.toString());
+    }
+}
